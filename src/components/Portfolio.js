b3a868288ff10a8a6013c88a34852189ae619171
Frederick Morin <frederickmorin18@gmail.com>
2019-11-29 18:07:45 -0500

    Add 'see more' button

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -3,6 +3,7 @@ import { useStaticQuery, graphql } from 'gatsby';
 import Section from './Section';
 import PortfolioCard from './PortfolioCard';
 import Container from './Container';
+import Button from './Button';
 
 const Portfolio = () => {
   const {
@@ -45,6 +46,11 @@ const Portfolio = () => {
           {nodes.map((card) => (
             <PortfolioCard key={card.title} {...card} />
           ))}
+          <div className="max-w-xs min-w-xs sm:max-w-sm md:max-w-xs lg:max-w-sm rounded overflow-hidden shadow-lg hover:shadow-2xl mb-8 mx-auto">
+            <Button link={'https://github.com/freddydumont'}>
+              See more on GitHub
+            </Button>
+          </div>
         </div>
       </div>
     </Section>
