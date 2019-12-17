84e65970be7a4272adaf0e863ed6571e18923c00
Frederick Morin <frederickmorin18@gmail.com>
2019-12-17 16:52:22 -0500

    Upgrade masonry layout

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Masonry from 'react-masonry-css';
 import { useStaticQuery, graphql } from 'gatsby';
 import Section from './Section';
 import PortfolioCard from './PortfolioCard';
@@ -39,18 +40,32 @@ const Portfolio = () => {
       <Container>
         <Section.Title>Portfolio</Section.Title>
       </Container>
-      <div className="w-full inline-block mx-auto">
-        <div className="col-1 md:col-2 md:max-w-2xl md:mx-auto lg:max-w-4xl xl:col-3 xl:max-w-7xl">
-          {nodes.map((card) => (
-            <PortfolioCard key={card.title} {...card} />
-          ))}
-          <div className="max-w-xs min-w-xs sm:max-w-sm md:max-w-xs lg:max-w-sm rounded overflow-hidden shadow-lg hover:shadow-2xl mb-8 mx-auto">
-            <Button link="https://github.com/freddydumont">
-              See more on GitHub
-            </Button>
-          </div>
-        </div>
-      </div>
+      <Masonry
+        breakpointCols={{
+          default: 3,
+          1279: 2,
+          767: 1,
+        }}
+        className="flex md:max-w-2xl md:mx-auto lg:max-w-4xl xl:max-w-7xl"
+      >
+        {nodes.map((card, index, arr) => {
+          // add the "see more" button with last item to avoid breaking the masonry library
+          if (index === arr.length - 1) {
+            return (
+              <React.Fragment key={card.title}>
+                <PortfolioCard {...card} />
+                <div className="max-w-xs min-w-xs sm:max-w-sm md:max-w-xs lg:max-w-sm rounded overflow-hidden shadow-lg hover:shadow-2xl mb-8 mx-auto">
+                  <Button link="https://github.com/freddydumont">
+                    See more on GitHub
+                  </Button>
+                </div>
+              </React.Fragment>
+            );
+          }
+
+          return <PortfolioCard key={card.title} {...card} />;
+        })}
+      </Masonry>
     </Section>
   );
 };
