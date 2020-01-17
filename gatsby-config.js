40e0f568c35c5a14588d0a578a83939ac8c244c0
Frederick Morin <frederickmorin18@gmail.com>
2020-01-17 17:00:54 -0500

    Remove gatsby-plugin-theme-u
    to avoid conflict with novela theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,6 @@ module.exports = {
     ],
   },
   plugins: [
-    'gatsby-plugin-theme-ui',
     'gatsby-plugin-advanced-sitemap',
     'gatsby-plugin-smoothscroll',
     `gatsby-transformer-sharp`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,10 @@ import About from '../components/About';
 import Portfolio from '../components/Portfolio';
 import Contact from '../components/Contact';
 import Home from '../components/Home';
+import ThemeProvider from '../components/ThemeProvider';
 
 const Index = () => (
-  <>
+  <ThemeProvider>
     <SEO
       title="freddydumont | Full stack web application development"
       description="I'm a full stack web application developer helping teams and startups turn ideas into reality. Click to find out how I can help you grow your business now!"
@@ -28,7 +29,7 @@ const Index = () => (
       <Portfolio />
       <Contact />
     </Box>
-  </>
+  </ThemeProvider>
 );
 
 export default Index;
