70fe9f884a3f6be86b98abef325e7ee0bf028254
Frederick Morin <frederickmorin18@gmail.com>
2020-05-13 21:58:20 -0400

    Style Select component

--- src/components/TagComboBox.js
+++ src/components/TagComboBox.js
@@ -1,19 +1,23 @@
-import React, { useContext } from 'react';
+/** @jsx jsx **/
+import { jsx, useThemeUI } from 'theme-ui';
+import { useContext } from 'react';
 import { Box, Label } from '@theme-ui/components';
 import Select from 'react-select';
 import { SelectedTagsContext } from './SelectedTagsProvider';
 
 const TagComboBox = () => {
   const [state, dispatch] = useContext(SelectedTagsContext);
+  const { theme: themeUI } = useThemeUI();
 
   return (
     <Box mb={12}>
-      <Label id="tag-select">Filter by technology tags</Label>
+      <Label id="tag-select" sx={{ pl: '2px' }}>
+        Filter by technology tags
+      </Label>
       <Select
         isMulti
         isSearchable
         isClearable
-        closeMenuOnSelect={false}
         name="tags"
         aria-labelledby="tag-select"
         options={state.tags}
@@ -22,6 +26,35 @@ const TagComboBox = () => {
             type: 'UPDATE_TAGS',
             updatedState,
           });
+        }}
+        theme={(theme) => ({
+          ...theme,
+          colors: {
+            // colors for components here:
+            // https://github.com/JedWatson/react-select/tree/master/packages/react-select/src/components
+            ...theme.colors,
+            primary25: themeUI.colors.primaryHover,
+            primary50: themeUI.colors.teal_vivid[3],
+            primary75: themeUI.colors.teal_vivid[4],
+            primary: themeUI.colors.primary,
+            danger: themeUI.colors.red_vivid[3],
+            dangerLight: 'transparent',
+            neutral0: themeUI.colors.gray[9],
+            neutral5: themeUI.colors.gray[8],
+            neutral10: themeUI.colors.gray[7],
+            neutral20: themeUI.colors.gray[6],
+            neutral30: themeUI.colors.gray[5],
+            neutral40: themeUI.colors.gray[4],
+            neutral50: themeUI.colors.gray[3],
+            neutral60: themeUI.colors.gray[2],
+            neutral70: themeUI.colors.gray[1],
+            neutral80: themeUI.colors.gray[0],
+            neutral90: themeUI.colors.gray[0],
+          },
+        })}
+        // text in dropdown inherits color set here
+        sx={{
+          color: 'gray.0',
         }}
       />
     </Box>
